f7c48ac96038735e4b4c548646c8de3304a5c2ba
vinoth <a.vinothk@outlook.com>
2015-10-11 18:18:25 +0530

    Pass update endpoint

--- app.rb
+++ app.rb
@@ -26,3 +26,90 @@ post '/passbooks' do
   gen_pass = passbook.file
   send_file(gen_pass.path, type: 'application/vnd.apple.pkpass', disposition: 'attachment', filename: "pass.pkpass")
 end
+
+module Passbook
+  class PassbookNotification
+    def self.register_pass(options)
+      status = verify_pass_and_token options
+      if status
+        return status
+      end
+
+      @device = Device.where(identifier: options['deviceLibraryIdentifier'], push_token: options['pushToken']).first_or_create
+      if Registration.find_by(pass_id: @pass.id, device_id: @device.id).present?
+        return {:status => 200}
+      else
+        Registration.create(pass_id: @pass.id, device_id: @device.id)
+        return {:status => 201}
+      end
+    end
+
+    def self.passes_for_device(options)
+      unless valid_device? options['deviceLibraryIdentifier']
+        return
+      end
+
+      update_tag = options['passesUpdatedSince'] || 0
+      passes = @device.passes.where('version > ?', update_tag.to_i)
+      if passes.present?
+        {'lastUpdated' => Time.now.utc.to_i.to_s, 'serialNumbers' => passes.map{|p| p.serial_number}}
+      else
+        return
+      end
+    end
+
+    def self.unregister_pass(options)
+      status = verify_pass_and_token options
+      if status
+        return status[:status] == 401 ? status : {:status => 200}
+      end
+
+      unless valid_device? options['deviceLibraryIdentifier']
+        return {:status => 401}
+      end
+
+      registrations = @device.registrations.where(pass_id: @pass.id)
+      if registrations.present?
+        registrations.destroy_all
+      end
+      return {:status => 200}
+    end
+
+    def self.latest_pass(options)
+      @pass = find_pass_with options['serialNumber']
+      unless @pass
+        return
+      end
+
+      passbook = Passbook::PKPass.new @pass.data.to_json.to_s
+      passbook.addFiles ['assets/logo.png', 'assets/logo@2x.png', 'assets/icon.png', 'assets/icon@2x.png']
+      {:status => 200, :latest_pass => passbook.stream.string, :last_modified => Time.now.utc.to_i.to_s}
+    end
+
+    def self.passbook_log(log)
+      log.values.flatten.compact.each do |l|
+        Log.create(log: l)
+      end
+    end
+  end
+end
+
+def verify_pass_and_token options
+  token = ENV['AUTH_TOKEN']
+  @pass = find_pass_with options['serialNumber']
+  if options['authToken'] != token
+    return {:status => 401}
+  elsif !@pass
+    return {:status => 404}
+  else
+    return
+  end
+end
+
+def find_pass_with serial
+  Pass.find_by(serial_number: serial)
+end
+
+def valid_device? identifier
+  @device = Device.find_by(identifier: identifier)
+end
