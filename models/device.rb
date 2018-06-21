758b4fc55dce278fed864142eaf6fecab94ee5cc
rhunt222 <richard.hunt2@gmail.com>
2018-06-21 10:35:35 -0400

    device_identifier to identifier
    Check uniqueness of 'Identifier' not undefined 'device_identifier'

--- models/device.rb
+++ models/device.rb
@@ -1,5 +1,5 @@
 class Device < ActiveRecord::Base
-  validates_uniqueness_of :device_identifier
+  validates_uniqueness_of :identifier
   validates_uniqueness_of :push_token
 
   has_many :registrations
